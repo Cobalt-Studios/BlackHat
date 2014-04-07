1454bf970d912479bb231f2ad586bebe3a63821b
TylerHadd <tylerhaddaway@hotmail.com>
2014-04-07 12:45:33 -0400

    Rearranging buttons.

--- BlackHat/STUDIO PROJECT/BlackHat/Form1.Designer.cs
+++ BlackHat/STUDIO PROJECT/BlackHat/Form1.Designer.cs
@@ -28,33 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
-            this.textBox1.Font = new System.Drawing.Font("Airstrike Academy", 71.99999F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.textBox1.Location = new System.Drawing.Point(0, 2);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1100, 89);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Black Hat";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Font = new System.Drawing.Font("Airstrike Academy", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.button1.Location = new System.Drawing.Point(0, 86);
+            this.button1.Location = new System.Drawing.Point(-10, 86);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(438, 137);
+            this.button1.Size = new System.Drawing.Size(448, 137);
             this.button1.TabIndex = 1;
             this.button1.Text = "Create Character";
             this.button1.UseVisualStyleBackColor = false;
@@ -67,7 +53,7 @@
             this.button2.ForeColor = System.Drawing.Color.DodgerBlue;
             this.button2.Location = new System.Drawing.Point(434, 86);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(666, 137);
+            this.button2.Size = new System.Drawing.Size(674, 137);
             this.button2.TabIndex = 2;
             this.button2.Text = "Load Character";
             this.button2.UseVisualStyleBackColor = false;
@@ -79,7 +65,7 @@
             this.button3.ForeColor = System.Drawing.Color.DodgerBlue;
             this.button3.Location = new System.Drawing.Point(-10, 410);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(1110, 220);
+            this.button3.Size = new System.Drawing.Size(1118, 233);
             this.button3.TabIndex = 3;
             this.button3.Text = "Quit Game";
             this.button3.UseVisualStyleBackColor = false;
@@ -94,18 +80,15 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Load Character";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
