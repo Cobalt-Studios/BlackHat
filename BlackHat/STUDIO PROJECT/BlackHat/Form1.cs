c7e1b7d6da65d8c6f50814db9c5036f24653ffa7
carterlinton96 <youngest2tallest@gmail.com>
2014-04-08 13:10:22 -0400

    commit to undeletion
    blah

--- BlackHat/STUDIO PROJECT/BlackHat/Form1.cs
+++ BlackHat/STUDIO PROJECT/BlackHat/Form1.cs
@@ -22,9 +22,16 @@ namespace BlackHat
 
         }
 
+
+        //QUITS THE GAME
         private void button3_Click(object sender, EventArgs e)
         {
+            Close();
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
